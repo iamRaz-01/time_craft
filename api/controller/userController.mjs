5e4a129cfcc6a5b01ab20e01032f3abb744bb4f8
Susikumar-Pitchaimuthu <susikumar.pitchaimuthu@gmail.com>
2023-12-27 17:42:04 +0530

    SignIn and SignUp CSS Added

--- api/controller/userController.mjs
+++
@@ -1,7 +0,0 @@
-import { app } from "../app.mjs";
-// import UserDao from "../dao/userDao.mjs";
-app.get('/api/timecraft/user/create',(req,res)=>{
-    // let data = req.body;
-    // const userDao =  new UserDao();
-     res.json({mess:"success"})
-})
